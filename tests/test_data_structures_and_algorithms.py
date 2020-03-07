f804b740ccd10343ca4e960ddd63dd23c91cea03
JB <jb.tellez@gmail.com>
2020-03-06 16:56:13 -0800

    comments out purposely failing test

--- tests/test_data_structures_and_algorithms.py
+++ tests/test_data_structures_and_algorithms.py
@@ -4,5 +4,5 @@ from data_structures_and_algorithms import __version__
 def test_version():
     assert __version__ == "0.1.0"
 
-def test_fail_on_purpose_to_test_ci():
-    assert False
+# def test_fail_on_purpose_to_test_ci():
+#     assert False
